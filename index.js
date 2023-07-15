f898fe3b98e0de5bf8c8e50ee3de5fd700e4c469
alexander white <whiteaj36@gmail.com>
2023-07-15 16:09:37 -0400

    fix things

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const csv = require('csvtojson')
 
 function main() {
-    const masterEmailFileName = './emails3.csv'
-    const testEmailFileName = './emails1.csv'
+    const masterEmailFileName = './csvs/emails3.csv' // NICK: Change this to whatever your master email list is
+    const testEmailFileName = './csvs/emails1.csv' // NICK: Change this to wahtever you want to remove duplicates in here
 
     csv({delimiter:','})
     .fromFile(masterEmailFileName)
@@ -11,22 +11,22 @@ function main() {
         csv({delimiter:','})
         .fromFile(testEmailFileName)
         .then((testEmails) => {
-            console.log(testEmails, 'testEmails')
             // filter emails1JSON and remove the object if it matches something in json
             testEmails.forEach((testEmail) => {
+                let matchFound = false
                 masterEmails.forEach((masterEmail) => {
-                    
-                    if (testEmail.Email === masterEmail.email) {
-                        console.log('WE MATCHED')
-
-                    }
-                    else {
-                        passedEmails.push(testEmail)
+                    if (testEmail.email === masterEmail.email) { // NICK: these lines do the comparison between the two values. 
+                        // testEmail and masterEmail have each line in the csv, 
+                        // and you can access the values by doing testEmail.email to get emails, or testEmail.name to get names
+                        matchFound=true
                     }
                 })
+                if (!matchFound) {
+                    passedEmails.push(testEmail)
+                }
             })
+
+        console.log(passedEmails.length)
         })
-        // console.log(passedEmails)
-    // return entries;
 })}
 main()
